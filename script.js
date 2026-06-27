3037a585e5b06aaa050001f5eac598f011d71920
Zayne <zaynetutor@gmail.com>
2026-06-27 17:10:50 +0200

    Added register page and navigation

---
+++ script.js
@@ -0,0 +1,9 @@
+const loginButton = document.querySelector(".login-btn");
+
+loginButton.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    alert("Welcome to ZayneChat!");
+
+});
